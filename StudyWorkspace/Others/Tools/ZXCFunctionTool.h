7ce4d03a9a5cdb97417783a2583be8211ef6310e
maowangxin <maowangxin@globalegrow.com>
2021-04-08 15:45:38 +0800

    add

--- StudyWorkspace/Others/Tools/ZXCFunctionTool.h
+++ StudyWorkspace/Others/Tools/ZXCFunctionTool.h
@@ -12,47 +12,47 @@
 
 @interface ZXCFunctionTool : NSObject
 
-/** 获取直播SDK Bundle */
-NSBundle* WXBundle(void);
-
 /** 获取直播SDK版本 */
-NSString* WXBundleVersion(void);
+NSString* ZXBundleVersion(void);
 
-/** Live SDK统一获取图片方法 */
-UIImage* WXImageName(NSString *imageName);
+/** 统一获取图片方法 */
+UIImage* ZXImageName(NSString *imageName);
 
 /** 给图片设置颜色 */
-UIImage* WXConvertImageColor(UIImage *image, UIColor *color);
+UIImage* ZXConvertImageColor(UIImage *image, UIColor *color);
 
 /// 高斯模糊图片
-UIImage* WXImageBlurry(UIImage *originImage, CGFloat blurLevel);
+UIImage* ZXImageBlurry(UIImage *originImage, CGFloat blurLevel);
 
 /** 根据颜色绘制图片 */
-UIImage * WXCreateImageWithColor(UIColor *color);
-
-
-#pragma mark -==================================多语言====================================
-
-/** 获取多语言 */
-NSString* WXLanguage(NSString *languageKey);
-
-/** 获取指定国家的多语言 */
-NSString* WXLanguageCountry(NSString *languageKey, NSString *countryCode);
+UIImage * ZXCreateImageWithColor(UIColor *color);
 
 
 #pragma mark -==================================颜色====================================
 
 /** 获取白颜色 */
-UIColor* WXColorWhite(void);
+UIColor* ZXColorWhite(void);
+
+/** 线条颜色 */
+UIColor* ZXColorLineColor(void);
+
+/** 黑色字体颜色:4A4A4A */
+UIColor* ZXColorBlackTextColor(void);
+
+/** App主黄色: F29448 */
+UIColor* ZXColorMainColor(void);
 
 /** 获取RGB颜色 */
-UIColor* WXColorRGB(NSInteger R, NSInteger G, NSInteger B, CGFloat a);
+UIColor* ZXColorRGB(NSInteger R, NSInteger G, NSInteger B, CGFloat a);
 
 /** 获取十六进制颜色 */
-UIColor* WXColorHex(NSInteger hexValue, CGFloat a);
+UIColor* ZXColorHex(NSInteger hexValue);
+
+/** 获取十六进制颜色, a:透明度 */
+UIColor* ZXColorHex_a(NSInteger hexValue, CGFloat a);
 
 /** 获取随机颜色 (调试时用到) */
-UIColor* WXColorRandom(void);
+UIColor* ZXColorRandom(void);
 
 /** 十六进制的颜色（以#/0X开头）转换为UIColor */
 UIColor* colorWithHexString(NSString *colorString);
@@ -62,62 +62,67 @@ UIColor* colorWithHexString(NSString *colorString);
 #pragma mark -==================================字体操作====================================
 
 /** 获取系统粗体字 */
-UIFont * WXFontBold(CGFloat size);
+UIFont* ZXFontSystem(CGFloat size);
 
 /** 获取系统粗体字 */
-UIFont * WXFontSystem(CGFloat size);
+UIFont* ZXFontBold(CGFloat size);
 
 /** 获取系统自定义字体 */
-UIFont * WXFontCustom(NSString *fontName, CGFloat size);
+UIFont* ZXFontCustom(NSString *fontName, CGFloat size);
 
+/** 数字等宽字体 (eg: 倒计时数字不抖动) */
+UIFont* ZXFontMonospacedDigit(CGFloat size);
 
 
 #pragma mark -==================================HUD====================================
 
-/** 获取弹框window */
-UIWindow * WXFetchHUDSuperView(void);
-
 /** 显示loading框现在window上禁止交互 */
-void WXShowLoadingView(void);
+void ZXShowLoadingToView(id parmaters);
 
 /** 隐藏window上的loading框 */
-void WXHideLoadingView(void);
+void ZXHideLoadingFromView(id parmaters);
 
 /** 显示纯文本Toast展示 */
-void WXShowToastWithText(NSString *message);
+void ZXShowToastWithText(id parmaters, NSString *message);
 
 /** 系统菊花转圈 */
-UIActivityIndicatorView * WXFetchIndicatorView(UIActivityIndicatorViewStyle style, UIColor *color);
+UIActivityIndicatorView * ZXFetchIndicatorView(UIActivityIndicatorViewStyle style, UIColor *color);
 
 
 #pragma mark -==================================字符串、对象====================================
 
 /**  判断是否为NSDictionary */
-BOOL WXJudgeNSDictionary(id obj);
+BOOL ZXJudgeNSDictionary(id obj);
 
 /** 判断是否为NSArray */
-BOOL WXJudgeNSArray(id obj);
+BOOL ZXJudgeNSArray(id obj);
 
 /** 判断字符串是否为空 */
-BOOL WXIsEmptyString(id obj);
+BOOL ZXIsEmptyString(id obj);
 
 /** 判断是否为NSString */
-BOOL WXJudgeNSString(id obj);
+BOOL ZXJudgeNSString(id obj);
 
 /** 转化为NSString来返回，如果为数组或字典转为Json String返回, 其他对象则返回@"" */
-NSString* WXToString(id obj);
+NSString* ZXToString(id obj);
+
+/// 格式化字符串
+NSString* ZXFormatString(NSString *format, ...);
 
 ///转换为字典
-NSDictionary * WXToDictiontry(id objc);
+NSDictionary* ZXToDictiontry(id objc);
 
 ///词典转换为字符串
-NSString * WXDictionaryToJson(NSDictionary *dic);
+NSString* ZXDictionaryToJson(NSDictionary *dic);
 
 /// 转成MD5字符串
-NSString * WXToMD5String(NSString *originStr);
+NSString* ZXToMD5String(NSString *originStr);
 
 
+///时间戳变为格式时间
+NSString *ZXConvertTimeStamp(NSTimeInterval timeStamp, NSString *dateFormat);
 
+    
 #pragma mark -==================================系统弹框操作====================================
 /** 跳转到系统授权设置页面 */
 void jumpApplicationOpenSetting(void);
@@ -130,28 +135,24 @@ void showAlertController(NSString *title, NSString *message,
                          NSString *otherTitle, void(^otherBtnBlock)(void),
                          NSString *cancelTitle, void(^cancelBtnBlock)(void));
 
+/**
+ *  字符串编码
+ *  @param originStr 源字符串
+ *  @param reservedSymbol 是否保留特殊字符不编码
+ */
+NSString *ZXCodingString(NSString *originStr, BOOL reservedSymbol);
 
-#pragma mark -==================================权限操作判断====================================
+/**
+ *  字符串解码
+ */
+NSString *ZXDecodeString(NSString *originStr);
 
-/** 相机是否可用 (1.没申请过权限 2.已申请且已关闭) */
-BOOL isCanUseCamera(void);
+#pragma mark -======== 播放系统声音 ========
 
-/** 是否已经申请过相机权限 */
-BOOL hasApplyCameraPermission(void);
-
-/** 主动申请相机权限 */
-void applyCameraPermission(void (^applyPermission)(BOOL));
-
-
-
-/** 麦克风是否可用 (1.没申请过权限 2.已申请且已关闭) */
-BOOL isCanUseMicrophone (void);
-
-/** 是否已经申请过麦克风权限 */
-BOOL hasApplyMicrophonePermission(void);
-
-/** 主动申请麦克风权限 */
-void applyMicrophonePermission(void (^applyPermission)(BOOL));
+/**
+ * 播放系统声音: http://iphonedevwiki.net/index.php/AudioServices
+ */
+- (void)playSystemSound:(UInt32)soundID;
 
 
 /** 修复一个奇葩的bug: (方法2)
